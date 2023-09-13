53748cdab5d838718be5daaaf3297737029f937b
IrfanFattekhan <irfan.fattekhan1991@gmail.com>
2023-09-13 22:52:28 +0530

    Heading component imported in App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css';
 import { Greet } from './components/Greet';
+import Heading from './components/Heading';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
@@ -23,7 +24,8 @@ function App() {
       {/* <Greet name='Ziya' age={3} isLoggedIn={true}/>
       <Person name={person}/>
       <PersonList names={personList}/> */}
-      <Status status='error'/>
+      {/* <Status status='error'/> */}
+      <Heading>PlaceHolder text</Heading>
     </div>
   );
 }
